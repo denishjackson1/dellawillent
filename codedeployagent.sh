efb42e707ce05c42e46a7f8d73b91f852da24869
denishjackson1 <denishkomakech@gmail.com>
2023-08-13 19:11:53 +0300

    codedeploy

--- codedeployagent.sh
+++ codedeployagent.sh
@@ -4,20 +4,20 @@ sudo apt install ruby-full
 sudo apt install wget
 cd /home/ubuntu
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
-sudo systemctl enable codedeploy-agent
-sudo service codedeploy-agent start
-sudo apt install -y python3-pip
-sudo pip install awscli
-
-#!/bin/bash
-sudo yum -y update
-sudo yum -y install ruby
-sudo yum -y install wget
-cd /home/ec2-user
-wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
-sudo yum install -y python-pip
-sudo pip install awscli
+# sudo systemctl enable codedeploy-agent
+# sudo service codedeploy-agent start
+# sudo apt install -y python3-pip
+# sudo pip install awscli
+
+# #!/bin/bash
+# sudo yum -y update
+# sudo yum -y install ruby
+# sudo yum -y install wget
+# cd /home/ec2-user
+# wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+# sudo chmod +x ./install
+# sudo ./install auto
+# sudo yum install -y python-pip
+# sudo pip install awscli
